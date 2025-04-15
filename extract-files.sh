de3d2a2eb3912ad79b1172d4164b9f81f24ab80e
Sabar <31942277+sabarop@users.noreply.github.com>
2025-04-15 11:16:05 +0700

    platina: DISABLE_CHECKELF -> FIX_SONAME

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,28 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/hw/fingerprint.fpc.so)
+            [ "$2" = "" ] && return 0
+           "${PATCHELF}" --set-soname "fingerprint.fpc.so" "fingerprint.fpc.default.so" "${2}"
+            ;;
+        vendor/lib64/hw/fingerprint.goodix.so)
+            [ "$2" = "" ] && return 0
+           "${PATCHELF}" --set-soname "fingerprint.goodix.so" "fingerprint.goodix.default.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
